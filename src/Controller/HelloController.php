069076cccf6836ce1f442768e43af36f124e6669
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-22 17:50:14 -0300

    Rota de Caninos

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -48,4 +48,10 @@ class HelloController {
     {
         return new Response('Olá, felino!');
     }
+
+    #[Route('/caninos')]
+    public function caninos(): Response
+    {
+        return new Response('Olá, canino!');
+    }
 }
