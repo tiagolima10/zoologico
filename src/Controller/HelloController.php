3091b1af763e921229bf6ad31b014dfe09b91aa0
Tiago Lima da Anunciação <tiagoanunc@gmail.com>
2024-09-22 17:06:48 -0300

    Criando minhas primeiras rotas

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,8 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController {
 
     #[Route('/')]
-    public function index() 
+    public function index(): Response
     {
-        return new Response('');
+        return new Response('Olá, Mundo!');
+    }
+
+    #[Route('/interna')]
+    public function interna(): Response
+    {
+        return new Response('Página interna');
     }
 }
